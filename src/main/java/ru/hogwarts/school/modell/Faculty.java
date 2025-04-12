2e6a095205bd2920038d826a607b1435a5099f2e
Homework-10 <danilovsansan@gmail.com>
2025-04-12 19:30:34 +0300

    Создал модели

--- src/main/java/ru/hogwarts/school/modell/Faculty.java
+++ src/main/java/ru/hogwarts/school/modell/Faculty.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.modell;
 
+import java.util.Objects;
+
 public class Faculty {
     private Long id;
     private String name;
@@ -11,5 +13,46 @@ public class Faculty {
         this.id = counter++;
         this.name = name;
         this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Faculty faculty)) return false;
+        return Objects.equals(getId(), faculty.getId()) && Objects.equals(getName(), faculty.getName()) && Objects.equals(getColor(), faculty.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getColor());
     }
 }
--- src/main/java/ru/hogwarts/school/modell/Student.java
+++ src/main/java/ru/hogwarts/school/modell/Student.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.modell;
 
+import java.util.Objects;
+
 public class Student {
     private Long id;
     private String name;
@@ -11,5 +13,46 @@ public class Student {
         this.id = counter++;
         this.name = name;
         this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student student)) return false;
+        return getAge() == student.getAge() && Objects.equals(getId(), student.getId()) && Objects.equals(getName(), student.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getAge());
     }
 }
